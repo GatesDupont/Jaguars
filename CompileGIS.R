6546de059fb83d07a317e76175548afc6f5b0138
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-07-09 19:32:46 -0400

    Update CompileGIS.R

--- CompileGIS.R
+++ CompileGIS.R
@@ -16,8 +16,10 @@ library(elevatr)
 select <- dplyr::select
 projection <- raster::projection
 
+
 #----Full BBox----
-df = expand.grid(data.frame(lat = c(-56.4, 37.9), long = c(-124.7,-32.9)))
+
+df = expand.grid(data.frame(lat = c(-60, 40), long = c(-125,-30)))
 
 # sp extent
 spdf = df
@@ -28,14 +30,18 @@ ext = st_as_sf(df, coords = c("long", "lat")) %>%
   st_set_crs(4326)
 ext_grid = st_make_grid(ext, cellsize = 4.5, what="centers")
 
+
 #----File to save spatial data----
+
 gpkg_dir <- "data/GIS"
 if (!dir.exists(gpkg_dir)) {
   dir.create(gpkg_dir)
 }
 f_ne <- file.path(gpkg_dir, "gis-data.gpkg")
 
+
 #----Political boundaries----
+
 # land border with lakes removed
 ne_land <- ne_download(scale = 50, category = "cultural",
                        type = "admin_0_countries_lakes",
@@ -52,6 +58,7 @@ ne_country_lines <- ne_download(scale = 50, category = "cultural",
 ne_country_lines <- st_intersects(ne_country_lines, ne_land, sparse = FALSE) %>%
   as.logical() %>%
   {ne_country_lines[.]}
+
 
 #----Elevation----
 
@@ -81,8 +88,10 @@ for(i in 2:length(elev.tiles)){
 }
 
 # Mosaicking tiles together
-call4mosaic = paste0("mosaic(", elev.tiles.names, ", fun=mean, filename='data/GIS/srtm/srtm.tif')")
+call4mosaic = paste0("mosaic(", elev.tiles.names, 
+                     ", fun=mean, filename='data/GIS/srtm/srtm.tif', overwrite=TRUE)")
 r_srtm = eval(parse(text = call4mosaic))
+srtm = crop(r_srtm, spdf)
 
 
 #----Landcover----
@@ -105,12 +114,32 @@ new_names <- format(as.Date(names(tifs)), "%Y") %>%
   file.path(dirname(tifs), .)
 file.rename(tifs, new_names)
 
+
 #----Canopy Height----
 
-CanopyHeight = raster("~/Desktop/GlobalVeg.tif") %>%
-  crop(., spdf) # downloaded from https://landscape.jpl.nasa.gov/
+library(R.utils)
+
+# make directory
+ch_dir = "data/GIS/canopyHeight"
+if (!dir.exists(ch_dir)) {
+  dir.create(ch_dir)
+}
+
+# download file from online
+download.file("https://landscape.jpl.nasa.gov/resources/Simard_Pinto_3DGlobalVeg_JGR.tif.gz",
+              destfile = paste0(ch_dir, "/Simard_Pinto_3DGlobalVeg_JGR.tif.gz"))
+
+# unzip the file
+gunzip("data/GIS/canopyHeight/Simard_Pinto_3DGlobalVeg_JGR.tif.gz")
+
+# read as raster and crop
+canopy_height = "data/GIS/canopyHeight/Simard_Pinto_3DGlobalVeg_JGR.tif" %>%
+  raster() %>%
+  crop(., spdf)
+
 
 #---Climate----
+
 # Pulling all worldclim data
 worldclim = getData("worldclim", var="bio", res = 2.5) # 2.5 minutes of a degree (about 4.5 km at eq)
 tmin = getData("worldclim", var="tmin", res = 2.5)
@@ -120,6 +149,44 @@ prec = getData("worldclim", var="prec", res = 2.5)
 # stacking and cropping worldclim
 climate = stack(worldclim, tmin, tmax, prec) %>%
   crop(., spdf)
+
+
+#----Human density----
+
+# make directory
+hd_dir = "data/GIS/humanDensity"
+if (!dir.exists(hd_dir)) {
+  dir.create(hd_dir)
+}
+
+hd00 = "https://sedac.ciesin.columbia.edu/downloads/data/gpw-v4/gpw-v4-population-density-rev11/gpw-v4-population-density-rev11_2000_30_sec_tif.zip"
+hd05 = "https://sedac.ciesin.columbia.edu/downloads/data/gpw-v4/gpw-v4-population-density-rev11/gpw-v4-population-density-rev11_2005_30_sec_tif.zip"
+hd10 = "https://sedac.ciesin.columbia.edu/downloads/data/gpw-v4/gpw-v4-population-density-rev11/gpw-v4-population-density-rev11_2010_30_sec_tif.zip"
+hd15 = "https://sedac.ciesin.columbia.edu/downloads/data/gpw-v4/gpw-v4-population-density-rev11/gpw-v4-population-density-rev11_2015_30_sec_tif.zip"
+
+
+hd = vector("list", 4)
+for(i in hd){
+  download.file(i, destfile = "data/GIS/`human density`")
+}
+
+
+#----Human footprint----
+
+# make directory
+hf_dir = "data/GIS/humanFootprint"
+if (!dir.exists(hf_dir)) {
+  dir.create(hf_dir)
+}
+
+# download file directly from online
+download.file(
+  "https://sedac.ciesin.columbia.edu/downloads/data/wildareas-v3/wildareas-v3-2009-human-footprint/wildareas-v3-2009-human-footprint-geotiff.zip",
+  destfile = paste0(hf_dir, "/wildareas-v3-2009-human-footprint-geotiff.zip"))
+
+unzip(zipfile = paste0(hf_dir, "/wildareas-v3-2009-human-footprint-geotiff.zip"),
+      exdir = "wildareas-v3-2009-human-footprint-geotiff")
+
 
 #----output----
 unlink(f_ne)
