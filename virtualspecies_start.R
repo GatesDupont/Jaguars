c0ce99cbc159d5a97a235479bca77617ee9e8876
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-07-25 15:01:17 -0700

    Update virtualspecies_start.R

--- virtualspecies_start.R
+++ virtualspecies_start.R
@@ -1,22 +1,79 @@
 library(raster)
 library(virtualspecies)
+library(xgboost)
+library(dismo)
+library(randomForest)
+library(dplyr)
+library(purrr)
+predict = raster::predict
 
+#----Environmental covariate data----
 worldclim = getData("worldclim", var = "bio", res = 10)
 
-# Generate response functions
-my.parameters = formatFunctions(bio1 = c(fun = 'dnorm', mean = 250, sd = 50),
-                                 bio12 = c(fun = 'dnorm', mean = 4000, sd = 2000))
+wc = worldclim[[c("bio1", "bio12")]]
 
-# Generate virtual species
-my.first.species = generateSpFromFun(raster.stack = worldclim[[c("bio1", "bio12")]],
-                                      parameters = my.parameters,
-                                      plot = TRUE)
 
-pa = convertToPA(my.first.species, species.prevalence = 0.05, plot = TRUE)
+#----Response functions----
+params = formatFunctions(
+  bio1 = c(fun = 'dnorm', mean = 250, sd = 50),
+  bio12 = c(fun = 'dnorm', mean = 4000, sd = 2000)
+)
 
-# Sampling of distribution
-pa.points = sampleOccurrences(
-  my.first.species$suitab.raster,
-  n = 10,
+
+#----Virtual species----
+vsp = generateSpFromFun(
+  raster.stack = wc,
+  parameters = params,
+  plot = TRUE
+)
+
+
+#----Presence-absence distribution----
+pa = convertToPA(vsp, species.prevalence = 0.05, plot = TRUE)
+
+#----Presence-absence sampling----
+pa.pts = sampleOccurrences(
+  pa,
+  n = 250,
+  sample.prevalence = 0.5,
   type = "presence-absence"
 )
+
+
+#----Extract enviornmental covariates----
+lc = extract(wc, pa.pts$sample.points[,c(1,2)], df=T)
+
+
+#----Assosciate covariates----
+df = cbind(Observed = pa.pts$sample.points[,c(4)], lc[,c(2,3)])
+
+
+#----Split train/test----
+# Random 60/40 split
+samp = sample(nrow(df), round(0.6 * nrow(df)))
+train = (df[samp,])
+test = (df[-samp,])
+
+
+#----Running the model----
+sdm = randomForest(Observed~bio1+bio12, train)
+evaluate(test[test$Observed==1,], test[test$Observed==0,], sdm)
+
+
+#----Predicting from raster stack----
+sdm_pred = predict(wc, sdm)
+
+
+#----Plotting true distribution and modeled----
+sdm.plot = function(raster, title){
+  image(raster, main = title,
+        col = rev(terrain.colors(100)),
+        xaxs = "i", xaxt = 'n', yaxt = 'n', ylab = "", xlab = "",
+        ann = T, asp = 1, axes = FALSE)
+}
+
+
+par(mfrow=c(1,2))
+sdm.plot(vsp$suitab.raster, title = "True")
+sdm.plot(sdm_pred, title = "Modeled")
+par(mfrow=c(1,1))
