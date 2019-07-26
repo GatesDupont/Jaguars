5890c035fce421a569d1f580163c667a0e547291
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-07-26 11:05:55 -0700

    Update mosaic_MODIS_tifs.R

--- CompileGIS/mosaic_MODIS_tifs.R
+++ CompileGIS/mosaic_MODIS_tifs.R
@@ -4,15 +4,16 @@ wd = setwd("C:\\Users\\MTobler\\Documents\\Jaguar\\data\\GIS\\modis\\tiles_compl
 
 ls = list.files()
 
+years = 2001:2016
 r_modis = vector("list", 16)
-for(j in 2001:2016){
-  rl = vector("list", 42)
-  for(i in 1:42){
+for(j in 1:length(years)){
+  rl = vector("list", 43)
+  for(i in 1:43){
     p2r = file.path(wd, ls[[i]])
     
     rl[[i]] = p2r %>%
       list.files() %>%
-      pluck(grep(as.character(j), .)) %>%
+      pluck(grep(as.character(years[j]), .)) %>%
       file.path(p2r, .) %>%
       raster(.)
   }
@@ -20,14 +21,20 @@ for(j in 2001:2016){
   
   # create string of raster names
   rl.names = "rl[[1]]"
-  for(i in 2:42){
+  for(i in 2:43){
     rl.names = paste0(rl.names, ", ", "rl[[", as.character(i), "]]")
   }
   
   # Mosaicking tiles together
   call4mosaic = paste0("mosaic(", rl.names, 
-                       ", fun=mean, filename='C:/Users/MTobler/Documents/Jaguar/data/GIS/modis/tiles_complete/modis_mcd12q1_", j, ".tif ', overwrite=TRUE)")
+                       ", fun=mean)")
   r_modis[[j]] = eval(parse(text = call4mosaic))
+  writeRaster(x = r_modis[[j]], filename = file.path(wd, paste0("modis_mcd12q1_jaguar_", years[j], ".tif")))
 }
 
-# missed V12 H11
+
+
+rf = vector("list", 16)
+for(i in 1:length(years)){
+  rf[[i]] = raster(file.path(wd, paste0("modis_mcd12q1_jaguar_", years[j], ".tif")))
+}
