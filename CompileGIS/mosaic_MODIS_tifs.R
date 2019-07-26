66bc58b078ec9828633dc1deada2e3b8f770cb64
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2019-07-26 11:24:34 -0700

    Update mosaic_MODIS_tifs.R

--- CompileGIS/mosaic_MODIS_tifs.R
+++ CompileGIS/mosaic_MODIS_tifs.R
@@ -9,7 +9,7 @@ r_modis = vector("list", 16)
 for(j in 1:length(years)){
   rl = vector("list", 43)
   for(i in 1:43){
-    p2r = file.path(wd, ls[[i]])
+    p2r = file.path(wd, ls[i])
     
     rl[[i]] = p2r %>%
       list.files() %>%
@@ -34,7 +34,20 @@ for(j in 1:length(years)){
 
 
 
+
+
+
+
+
 rf = vector("list", 16)
 for(i in 1:length(years)){
-  rf[[i]] = raster(file.path(wd, paste0("modis_mcd12q1_jaguar_", years[j], ".tif")))
+  rf[[i]] = raster(file.path(wd, paste0("modis_mcd12q1_jaguar_", years[i], ".tif")))
 }
+
+
+plot(rf[[1]])
+plot(rf[[2]])
+plot(rf[[3]])
+
+
+test = overlay(rf[[1]], rf[[16]], fun=function(r1,r2){return(r2-r1)})
